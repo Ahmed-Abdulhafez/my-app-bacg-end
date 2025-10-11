cd5af461da2f660d7c0b4a50b6d286851bfc111b
unknown <alsheikhahmed996@gmail.com>
2025-10-11 03:54:27 +0300

    add

--- app.js
+++ app.js
@@ -37,11 +37,11 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 const connectDB = require('./config/db');
 connectDB();
 
-// module.exports = app;
+module.exports = app;
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
