cdb18df9fc6e218aac715679388edfa4285ddb49
unknown <alsheikhahmed996@gmail.com>
2025-10-14 21:15:15 +0300

    add new

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use("/users", require("./routes/Users"));
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+// module.exports = app;
