65752f9c258ce6c403f030e2886a715493f84570
unknown <alsheikhahmed996@gmail.com>
2025-11-03 10:45:34 +0200

    add new

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.use("/admin", require("./routes/admin"));
 app.use("/cart", require("./routes/cart"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-// module.exports = app;
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+module.exports = app;
