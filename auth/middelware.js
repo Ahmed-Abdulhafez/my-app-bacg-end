6f53840dfa9487b8eacf8a7218a162815a781bc1
unknown <alsheikhahmed996@gmail.com>
2025-11-03 11:54:13 +0200

    add

--- auth/middelware.js
+++ auth/middelware.js
@@ -28,24 +28,28 @@ const auth = (requiredRole = null) => {
       });
     } catch (error) {
       console.error("Auth middleware error:", error);
-      res.status(500).json({ msg: "Server error in auth middleware." });
+      res.status(401).json({ msg: "Server error in auth middleware." });
     }
   };
 };
 
 
-const cookieAuth = (req , res, next) => {
+const cookieAuth = (req, res, next) => {
   try {
     const token = req.cookies.token;
+
     if (!token) {
       return res.status(401).json({ msg: "Access denied. No token provided." });
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
+
     next();
   } catch (error) {
     console.error("❌ cookieAuth error:", error);
-    res.status(500).json({ msg: "Server error in cookieAuth." });
+    return res.status(401).json({ msg: "Invalid or expired token." });
   }
-}
+};
+
 module.exports = {auth , cookieAuth};
