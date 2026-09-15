932314e2a5f9a1d3490b5a85feed9346ae87caf1
Ahmed-Abdulhafez <121952479+Ahmed-Abdulhafez@users.noreply.github.com>
2026-09-15 11:17:28 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     console.log("🚀 Trying to connect to MongoDB...");
-    console.log("🔗 MONGO_URI:", process.env.MONGO_URI ? "✅ Loaded" : "❌ Missing");
+    console.log("🔗 MONGO_URI:", process.env.MONGO_DB_URL ? "✅ Loaded" : "❌ Missing");
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
